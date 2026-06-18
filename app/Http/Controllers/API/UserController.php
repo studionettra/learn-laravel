7bf080d601fa1410fb1bc9fed33882e1de5b3982
Studio Nettra <studionettra@gmail.com>
2026-06-18 08:31:47 +0700

    api laravel

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+
 class UserController extends Controller
 {
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\LoginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,5 +11,9 @@ Route::get('/user', function (Request $request) {
 Route::get('/', function () {
     return response()->json(['message' => 'API HAS RUN!']);
 });
+Route::post('login', [LoginController::class, 'login'])->name('login');
 
-Route::apiResource('user', \App\Http\Controllers\API\UserController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('user', \App\Http\Controllers\API\UserController::class);
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+});
